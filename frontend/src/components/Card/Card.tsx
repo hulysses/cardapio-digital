53824be21f0eb3a7808dddfe6059ff24a24c51bd
hulysses <hulyssesmaga@gmail.com>
2024-11-27 14:21:35 -0300

    add axios dependency and implement useFoodData hook for API integration; refactor App and Card components

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -12,7 +12,7 @@ export function Card(props: CardProps) {
       <img src={props.image} alt={`${props.title}`} />
       <h2>{props.title}</h2>
       <p>
-        <b>Valor:</b>
+        Valor: <b>{props.price}</b>
       </p>
     </div>
   );
---
+++ frontend/src/hooks/useFoodData.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosPromise } from "axios";
+import { FoodData } from "../interfaces/FoodData";
+import { useQuery } from "@tanstack/react-query";
+
+const API_URL = "http://localhost:8080";
+
+const fechData = async (): AxiosPromise<FoodData[]> => {
+  const response = axios.get(API_URL + "/food");
+  return response;
+};
+
+export function useFoodData() {
+  const query = useQuery({
+    queryFn: fechData,
+    queryKey: ["food-data"],
+    retry: 2,
+  });
+
+  return {
+    ...query,
+    data: query.data?.data,
+  };
+}
